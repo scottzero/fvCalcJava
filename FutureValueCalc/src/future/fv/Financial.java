72d3f339648d6b0572249e3d7f5ce9c4b738fa90
wolfeJob <spayton.c@gmail.com>
2017-11-20 23:16:49 -0700

    second commit, author revised

--- FutureValueCalc/src/future/fv/Financial.java
+++ FutureValueCalc/src/future/fv/Financial.java
@@ -1,5 +1,5 @@
 /*
-Author: Scott Payton
+Author: wolfeJob
 Assignment: Ex_21-2
 Date:11/19/2017 
 Description: In this exercise, you’ll add a Clear button to the GUI version of the Future Value Calculator. 
